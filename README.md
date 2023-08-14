54d5fe3af351edb8296467f9a76e3fd52ac39105
Rogier den Breejen <32416114+rbreejen@users.noreply.github.com>
2023-08-15 00:27:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Comparison of CDC data replication services
+
+[![plot](./www/assets/images/img.png)](https://rbreejen.github.io/comparison-dbms/www/table.html)
+
+## Disclaimer
+
+This research may exhibit errors. Please file a pull request to propose a change.
---
+++ create-table.R
@@ -0,0 +1,200 @@
+# 1. Load required packages -----------------------------------------------
+
+list.of.packages <- c("reactable", "data.table", "here", "htmltools", "htmlwidgets", "webshot", "stringr", "reactablefmtr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
+# 2. Retrieve data --------------------------------------------------------
+
+data <- fread(here::here("comparison-dbms.csv"),quote="", header=T)
+
+column_transformer <- function(value) {
+  string <- value
+  if (str_detect(value, ":n:")) {
+    string <- stringr::str_replace(value, ":n:", "<span><img alt='' src='./assets/icons/icon.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>")
+  } else if (str_detect(value, ":y:")) {
+    string <- stringr::str_replace(value, ":y:", "<span><img alt='' src='./assets/icons/62fc3b327bf0d9337241e112_check.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>")
+  } else if (str_detect(value, ":u:")) {
+    string <- stringr::str_replace(value, ":u:", "<span><img alt='' src='./assets/icons/62fc3b3276df8460a3e8d91b_output-onlinepngtools.png' style='width: 20.46px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>")
+  # } else if (!is.na(as.numeric(value))) {
+  #   span(class = "number", value)
+  }
+  markdown::mark(string)
+}
+
+tbl <- reactable(data, 
+                 pagination = FALSE,
+                 borderless=T,
+                 columnGroups = list(
+                   colGroup(name = "Market Leaders", columns = c("Oracle", "MySQL", "SQL server", "PostgreSQL","Databricks", "Snowflake")),
+                   colGroup(name = "Disruptors", columns = c("Clickhouse", "CockroachDB","Yugabyte", "TiDB", "SingleStore", "DuckDB"))
+                 ),
+                 defaultColDef = colDef(
+                   vAlign = "center",
+                   headerVAlign = "bottom",
+                   class = "cell",
+                   headerClass = "header"
+                 ),
+          columns = list(
+            item = colDef(
+              html = TRUE,
+              name = "",
+              align = "left",
+              #resizable = TRUE,
+              cell = function(value, index) {
+                # this is ugly but will fix later
+                title <- column_transformer(paste0(value, column_transformer(data[index, "description"][[1]])))
+                title <- as.character(div(class = "item-content-left", HTML(title)))
+                if (data[index, "is_advanced"][[1]]) {
+                  advanced_tag <- div(class = "item-content-right", span(class = "tag", "Advanced"))
+                  title <- paste0(title, tagList(advanced_tag))
+                }
+                title <- paste0("<div class = 'item-wrapper'>", title, "</div>")
+                title
+              },
+              minWidth = 280
+            ),
+            `Oracle` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>Oracle</div>"
+            ),
+            `MySQL` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>MySQL</div>"
+            ),
+            `SQL server` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>SQL server</div>"
+            ),
+            `PostgreSQL` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>PostgreSQL</div>"
+            ),
+            `Snowflake` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>Snowflake</div>"
+            ),
+            `Databricks` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>Databricks</div>"
+            ),
+            `Clickhouse` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>Clickhouse</div>"
+            ),
+            `CockroachDB` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>CockroachDB</div>"
+            ),            
+            `Yugabyte` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>Yugabyte</div>"
+            ),        
+            `TiDB` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>TiDB</div>"
+            ),
+            `SingleStore` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>SingleStore</div>"
+            ),            
+            `DuckDB` = colDef(
+              html=TRUE,
+              align = "center",
+              cell = function(value) column_transformer(value),
+              class = "border-left",
+              minWidth = 90,
+              name = "<div class='header-wrapper'>DuckDB</div>"
+            ),            
+            description = colDef(show = FALSE),
+            is_advanced = colDef(show = FALSE)
+          ),
+          highlight = F,
+          theme = reactableTheme(
+            #highlightColor = "#f3fafb",
+            #borderColor = "hsl(0, 0%, 93%)",
+            #padding = "2px",
+            headerStyle = list(borderColor = "hsl(0, 0%, 90%)"),
+            # Vertically center cells
+            cellStyle = list(display = "flex", flexDirection = "column", justifyContent = "center")
+          ),
+          defaultExpanded = T,
+          rowStyle = function(index) {
+            if (index %in% c(2:15, 19, 23:26, 31, 36:42)) list(backgroundColor = "rgb(247, 246, 235)")
+            else if (index %in% c(17, 21, 28:29, 33:34)) list(backgroundColor = "rgb(237, 241, 246)")
+          },
+          sortable = F,
+          class = "comparison-tbl")
+
+#Make sure the locale is set to English
+Sys.setlocale("LC_TIME", "C")
+
+tbl <- div(class = "comparison",
+    div(class = "comparison-header", id = 'header01',  tags$br(),
+        h2(class = "comparison-title", "Feature comparison Database Management Systems (DBMS) - WIP"),
+        paste0("Date of comparison: ", format(Sys.Date(), "%B")," 2023")
+    ),
+    tbl
+)
+
+tbl <- browsable(
+  tagList(list(
+    tags$head(
+      tags$link(href = "https://fonts.googleapis.com/css?family=Karla:400,700|Fira+Mono&display=fallback", rel = "stylesheet"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "./assets/styles/styles.css")
+    ),
+    tbl
+  ))
+)
+
+# Saving ------------------------------------------------------------------
+html_file <- "www/table.html"
+save_html(tbl, file = html_file, libdir = "lib")
+
+Sys.setenv(OPENSSL_CONF="/dev/null")
+webshot::webshot(url = html_file, file = "www/assets/images/img.png", delay = 0.1, vwidth = 2000)
